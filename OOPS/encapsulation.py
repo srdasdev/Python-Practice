c527b6cea7764b1e8824aa9816ec15c6a14ef41d
Soumya Ranjan Das <soumyaranjand295@gmail.com>
2026-06-16 22:39:30 +0530

    Add encapsulation.py to demonstrate encapsulation in Python with a BankAccount class

---
+++ OOPS/encapsulation.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, account_number, balance):
+        print("Initializing BankAccount...")
+        self.__account_number = account_number  # Private attribute
+        self.__balance = balance  # Private attribute
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+            print(f"Deposited: {amount}. New Balance: {self.__balance}")
+        else:
+            print("Deposit amount must be positive.")
+
+
+    def get_balance(self):
+        return self.__balance
+
+    def get_account_number(self):
+        return self.__account_number
+
+account = BankAccount("123456789", 1000)
+account.deposit(500)  # Depositing money to the account
+print(f"Account Number: {account.get_account_number()}")  # Accessing private attribute
+print(f"Initial Balance: {account.get_balance()}")  # Accessing private attribute
