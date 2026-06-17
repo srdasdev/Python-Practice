516083fb35789dbfe001c0bb7faa1c03e6d81d3e
Soumya Ranjan Das <soumyar.das1@hyscaler.com>
2026-06-17 17:24:08 +0530

    Add defaultdict import and prepare for word counting with defaultdict in count_words.py

--- count_words.py
+++ count_words.py
@@ -1,4 +1,6 @@
 from collections import Counter
+from collections import defaultdict
+
 
 corpus = ["the cat sat", "the cat ate", "the dog ran"]
 
@@ -7,7 +9,8 @@ get_each_word = " ".join(corpus).split()  # Split the corpus into individual wor
 # print(get_each_word)  # Output: ["the", "cat", "sat", "the", "cat", "ate", "the", "dog", "ran"]
 
 
- # List comprehension to flatten the list of words
+
+
 count = 0
 for data in get_each_word:
     print(data)
@@ -22,3 +25,11 @@ for data in get_each_word:
 
 print(dict)
 #  // Output: {'the': 3, 'cat': 2, 'sat': 1, 'ate': 1, 'dog': 1, 'ran': 1}
+
+
+
+# # using  default dict
+# grouped= defaultdict(list)
+
+# for data in get_each_word:
+#     grouped[data]= grouped.get(data, 0) + 1
